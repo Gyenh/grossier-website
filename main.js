8139c45620c3ca498195eab84264a28e062888d0
Gyenh <blazeog@hotmail.com>
2018-06-16 22:55:27 +0200

    Personnalisation avec ajout JS

---
+++ main.js
@@ -0,0 +1,43 @@
+function myFunction () {
+	document.body.style.backgroundColor = 'magenta';
+}
+
+function pblue() {
+    var x = document.getElementsByTagName("P");
+    var i;
+    for (i=0; i < x.length; i++) {
+    	x[i].style.color = "blue";
+    }
+
+}
+
+function hgreen() {
+	var x = document.querySelectorAll("h1, h2");
+	var i;
+	for (i=0; i < x.length; i++) {
+		x[i].style.color = "green";
+	}
+}
+
+function policepap() {
+		var x = document.querySelectorAll("h1, h2");
+	var i;
+	for (i=0; i < x.length; i++) {
+		x[i].style.fontFamily = "Comic Sans";
+	}
+}
+
+function policepap() {
+	var x = document.getElementsByTagName("P");
+	var i;
+	for (i=0; i < x.length; i++) {
+		x[i].style.fontFamily = "papyrus";
+	}
+}
+
+
+window.onload = function() {
+	setTimeout(function() {
+	alert('xxxvidsxxx : le meilleur du porno zoophile');
+	}, 5000);
+}
